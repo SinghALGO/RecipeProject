92f157a7c50a541ddfdfff9f79e4b9b247b131fb
brittanyshelley <brittany.j.shelley@gmail.com>
2024-02-12 13:59:09 -0700

    accepted change to recipe-list-image alt addition

--- recipehub/src/components/RecipeListItem/RecipeListItem.jsx
+++ recipehub/src/components/RecipeListItem/RecipeListItem.jsx
@@ -5,11 +5,7 @@ const RecipeListItem = ({ recipe, clickHandler}) => {
   };
   return (
     <section className="recipe-list__item" onClick={onRecipeClick}>
-<<<<<<< HEAD
-      <img className="recipe-list__image" src={recipe.img}></img>
-=======
       <img className="recipe-list__image" src={recipe.img} alt={recipe.name}></img>
->>>>>>> 96d9c6b2a41178eb3523e432b1d4d3e3e57900f2
       <div className="recipe-list__user-details">
         <p>{recipe.name}</p>
         <p>Cooking time: {recipe.cooking_time} mins</p>
